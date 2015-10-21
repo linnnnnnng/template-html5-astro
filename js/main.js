8f4710120c0cd227783a9f78297062c0d2818443
linnnnnnng <chinling.yeo@KULGCR-MBOP-142.local>
2015-10-21 14:52:59 +0800

    20151021

--- js/main.js
+++ js/main.js
@@ -34,9 +34,18 @@ $.address.init(function(event) {
  * All your scripts insert here
  * 
  */
+var isTablet, isMobile;
 $(function() {
 	console.log( "ready!" );
 	$('#anime').animate({padding:75}, 500);
+	
+	
+	isTablet = (/ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(navigator.userAgent.toLowerCase()));
+	
+	//mobile and tablet detection
+	if($.browser.mobile || isTablet){
+		//if is mobile or tablet
+	}
 });
 
 function goPage(sPage){
